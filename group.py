de8f8f10b3b7a9343f8bbc02ee4aa7308fbcdd76
alightbody <86264700+alightbody@users.noreply.github.com>
2021-11-29 14:19:57 -0500

    Add files via upload

---
+++ group.py
@@ -0,0 +1,27 @@
+# Collaborative code for ESCI 895.02
+
+# Lauren D
+
+# Ashley
+
+# Tim
+
+# Lauren K
+
+# Desmond
+
+# Reese
+
+# Emily
+
+# Lara
+
+# Deni
+
+# Julia
+
+# Megan
+
+# Annelise
+
+# Devan
